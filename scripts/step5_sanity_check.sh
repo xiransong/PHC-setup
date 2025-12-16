4adad13c8344aaa6081bcd6f25fb77f3bb3f10d9
xiransong <xiransong@outlook.com>
2025-12-16 11:07:33 -0500

    fix command

--- scripts/step5_sanity_check.sh
+++ scripts/step5_sanity_check.sh
@@ -1,9 +1,15 @@
 python phc/run_hydra.py \
-  learning=im \
-  env=env_im \
+  learning=im_mcp_big \
+  learning.params.network.ending_act=False \
+  exp_name=phc_comp_kp_2 \
+  env.obs_v=7 \
+  env=env_im_getup_mcp \
   robot=smpl_humanoid \
+  robot.real_weight_porpotion_boxes=False \
   env.motion_file=sample_data/amass_isaac_standing_upright_slim.pkl \
+  env.models=['output/HumanoidIm/phc_kp_2/Humanoid.pth'] \
+  env.num_prim=3 \
   env.num_envs=1 \
   headless=True \
-  test=True \
-  epoch=-1
+  epoch=-1 \
+  test=True
